8172b9b1a52c03b4663b4e4397d8bcb6553650cf
JWFreeStyle07 <jadeaerronmarges178@gmail.com>
2025-09-27 22:39:52 +0800

    include PHIL IRI passages

---
+++ app/algorithm/referenceTexts.tsx
@@ -0,0 +1,17 @@
+// referenceTexts.ts
+
+export const referenceTexts = [
+    {
+        title: `For Testing Only`,
+        text: `All will be good as time goes by. And you can pass this.`
+    },
+    {
+        title: `Telling Time`,
+        text: `Humans have used different objects to tell time. In the beginning, they used an hourglass. This is a cylindrical glass with a narrow center which allows sand to flow from its upper to its lower portion. Once all the sand has trickled to the lower portion, one knows that an hour has passed. Using the same idea, water clocks were constructed to measure time by having water flow through a narrow passage from one container to another. On the other hand, sundials allowed people to estimate an hour by looking at the position of the shadow cast by the sun on a plate. At night, people measured time by checking the alignment of the stars in the sky. None of these were accurate, though. The clock was the first accurate instrument for telling time`},
+    {
+        title: `Counting the Hours`,
+        text: `When men decided to divide the day into twenty-four hours, they used numbers one through twelve two times. As a result, there was one oclock during the day and another one oclock after midnight. This created confusion. If one was told to submit a project at six oclock, did this mean six oclock in the morning or at night? The Romans provided a solution to this problem. They thought that noon time, the time when the sun is at its apex, is an important time. They called noon Meridies and measured time by this. They called the morning ante meridiem, which means “before noon” while “after noon” was called post meridiem. Ante meridiem was shortened to A.M. while post meridiem was shortened to P.M.`},
+    {
+        title: `Nose Bleeds`,
+        text: `Having a nosebleed is a common occurrence. Children experience epistaxis when blood flows out from either or both nostrils, often for a short period of time. It may be caused by ones behavior like frequent nose picking or blowing too hard when one has a cold. It may also be caused by certain physical factors such as an allergy or abnormal growths in the nasal cavity. Or it may be due to environmental conditions such as exposure to toxic fumes or dryness of the air. While it is often thought that holding the head back can treat a nosebleed, this can actually cause one to choke or vomit. The best thing to do is to lean forward, pinch the top of the nose and apply a cold compress. And if that does not work, it is best to get professional help.`},
+];
